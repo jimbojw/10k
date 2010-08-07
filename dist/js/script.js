376c008064943d7b1c991a7f51bd7cedb35ae894
jimbojw <wilson.jim.r@gmail.com>
2010-08-07 06:12:26 -0400

    latest build

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,7 +3,7 @@
  */
 window['10kse'] = {};
 /**
- * scanner.js
+ * scanner.js - responsible for extracting potentially interesting content from the page.
  */
 (function(tenk){
 
@@ -12,53 +12,204 @@ window['10kse'] = {};
  */
 function scanner(window,document) {
 	
-	// initialization
+	// thanks ppk! http://www.quirksmode.org/dom/getstyles.html
+	var view = document.defaultView;
+	function getStyle(el,styleProp) {
+		var style;
+		if (el.currentStyle) {
+			style = el.currentStyle[styleProp];
+		} else if (window.getComputedStyle) {
+			style = view.getComputedStyle(el,null).getPropertyValue(styleProp);
+		}
+		return style;
+	}
+	
+	/**
+	 * determine whether a given element is visible
+	 */
+	function visible(elem) {
+		return getStyle(elem, 'display') !== 'none' && getStyle(elem, 'visiblity') !== 'hidden';
+	}
+	
+	// iterator vars
 	var
-		scan = [document.body],
+		i,
+		j,
+		l,
+		m,
+		n;
+	
+	// get selection - best clue as to the important content on the page
+	var selection = document.getSelection();
+	
+	// prioritize headings over other content
+	var
+		tags = 'h1,h2,h3,h4,h5,h6,pre,code'.split(','),
+		priority = [],
+		elems,
+		el;
+	for (i=0, l=tags.length; i<l; i++) {
+		elems = document.getElementsByTagName(tags[i]);
+		for (j=0, m=elems.length; j<m; j++) {
+			el = elems[j];
+			if (visible(el)) {
+				priority.push("<" + tags[i] + ">" + el.textContent);
+			}
+		}
+	}
+	
+	// initialize vars for content scan
+	var
+		
+		// full text content
+		content = [],
+		
+		// queue of elements left to scan, current element, and its children nodes
+		queue = [document.body],
 		elem,
 		children,
+		
+		// current child being evaluated and its nodeType
 		child,
 		type,
-		words,
-		text,
-		word,
-		empty = /^[^a-z]*$/,
+		
+		// test for interesting strings
+		interesting = /[a-z][a-z][a-z]/i,
+		
+		// tags to ignore
 		ignore = /button|link|noscript|script|style/i;
 	
-	var buf = [];
-	
-	// get selection - best clue
-	
-	// prioritize high-level tags (h1, etc)
-	
-	// everything else
-	var stop = /able|about|across|after|all|almost|also|among|and|any|are|because|been|but|can|cannot|could|dear|did|does|either|else|ever|every|for|from|get|got|had|has|have|her|hers|him|his|how|however|into|its|just|least|let|like|likely|may|might|most|must|neither|nor|not|off|often|only|other|our|own|rather|said|say|says|she|should|since|some|than|that|the|their|them|then|there|these|they|this|tis|too|twas|wants|was|were|what|when|where|which|while|who|whom|why|will|with|would|yet|you|your/i;
-	while (elem = scan.shift()) {
+	// scan document for content
+	while (elem = queue.shift()) {
 		children = elem.childNodes;
-		for (var i=0, l=children.length; i<l; i++) {
+		for (i=0, l=children.length; i<l; i++) {
 			child = children[i];
 			type = child.nodeType;
-			if (type === 1 && !(ignore).test(child.tagName)) {
-				scan.push(child);
+			if (
+				type === 1 && 
+				!(ignore).test(child.tagName) &&
+				getStyle(child, 'display') !== 'none' &&
+				getStyle(child, 'visibility') !== 'hidden'
+			) {
+				queue.push(child);
+			} else if (type === 3) {
+				text = child.nodeValue;
+				if ((interesting).test(text)) {
+					content.push(text);
+				}
+			}
+		}
+	}
+	
+	// send extracted data off for indexing
+	window['10kse'].iframe.contentWindow.postMessage(JSON.stringify({
+		selection: selection,
+		priority: priority,
+		content: content
+	}), "*");
+	
+}
+
+// export
+tenk.scanner = scanner;
+
+})(window['10kse']);
+
+/**
+ * indexer.js - responsible for extracting index worthy content from scanned content.
+ */
+(function(tenk){
+
+/**
+ * index interesting page content
+ */
+function indexer(content) {
+	
+	
+	// index user's selected content (if any)
+	
+	// index prioritized content
+	
+	// index full content
+	
+}
+
+// export
+tenk.indexer = indexer;
+
+/* scratch pad
+
+	// initialize vars for content scan
+	var
+		
+		// full text index and counter for document position
+		index = {},
+		count = 0,
+		
+		// queue of elements left to scan, current element, and its children nodes
+		queue = [document.body],
+		elem,
+		children,
+		
+		// current child being evaluated and its nodeType
+		child,
+		type,
+		
+		// string content of a TextNode and words in that text
+		text,
+		words,
+		word,
+		lc,
+		
+		// regular expression to test for uninteresting strings
+		empty = /^[^a-z]*$/,
+		
+		// tags to ignore
+		ignore = /button|link|noscript|script|style/i,
+		
+		// so-called "stop" words (uninteresting to search)
+		stop = /able|about|across|after|all|almost|also|among|and|any|are|because|been|but|can|cannot|could|dear|did|does|either|else|ever|every|for|from|get|got|had|has|have|her|hers|him|his|how|however|into|its|just|least|let|like|likely|may|might|most|must|neither|nor|not|off|often|only|other|our|own|rather|said|say|says|she|should|since|some|than|that|the|their|them|then|there|these|they|this|tis|too|twas|wants|was|were|what|when|where|which|while|who|whom|why|will|with|would|yet|you|your/i;
+	
+	// scan document for content
+	while (elem = queue.shift()) {
+		children = elem.childNodes;
+		for (i=0, l=children.length; i<l; i++) {
+			child = children[i];
+			type = child.nodeType;
+			if (
+				type === 1 && 
+				!(ignore).test(child.tagName) &&
+				getStyle(child, 'display') !== 'none' &&
+				getStyle(child, 'visibility') !== 'hidden'
+			) {
+				queue.push(child);
 			} else if (type === 3) {
 				text = child.nodeValue;
 				words = text.split(/[^0-9a-z_]+/i);
-				for (var j=0, m=words.length; j<m; j++) {
+				for (j=0, m=words.length; j<m; j++) {
+					count++;
 					word = words[j];
 					if (word.length > 2 && !(empty).test(word) && !(stop).test(word)) {
-						buf.push(word);
+						lc = word.toLowerCase();
+						var entry = index[lc];
+						if (!entry) {
+							entry = index[lc] = [];
+						}
+						entry.push(count);
 					}
 				}
 			}
 		}
 	}
 	
-	alert(buf.join(", "));
-	
-}
+	// send extracted data off for indexing
+	window['10kse'].iframe.contentWindow.postMessage(JSON.stringify({
+		selection: selection,
+		headings: headings,
+		index: index
+	}), "*");
 
-// export
-tenk.scanner = scanner;
+*/
 
 })(window['10kse']);
 
@@ -95,27 +246,38 @@ function readCookie(name) {
  */
 function bookmarklet(window,document,origin) {
 	
+	// setup namespace
+	var tenk = window['10kse'];
+	if (!tenk) {
+		tenk = window['10kse'] = {};
+	}
+	
+	// if script was previously retrieved, simple rexecute it
+	if (tenk.scanner) {
+		return tenk.scanner();
+	}
+	
 	// listen for script posted from origin and eval it
 	window.addEventListener("message", function (event) {
 		if (origin.substr(0, event.origin.length)===event.origin) {
-			(new Function(event.data))();
+			tenk.scanner = new Function(event.data);
+			tenk.scanner();
 		}
 	}, false);
 	
 	// open iframe to origin and style it
 	var
-		iframe = document.createElement('iframe'),
+		iframe = tenk.iframe = document.createElement('iframe'),
 		style = iframe.style;
 	iframe.setAttribute('src', origin);
-	style.width = style.height = "500px";
-	style.top = style.left = "100px";
+	style.width = style.height = "1px";
+	style.top = style.left = "-2px";
+	style.border = "none";
 	style.position = "absolute";
 	
 	// prepare listeners for iframe state changes, and
 	// when iframe is ready, use postMessage() to ask for script
-	var
-		done = false,
-		body = document.body;
+	var done = false;
 	iframe.onload = iframe.onreadystatechange = function() {
 		if (!done && (!this.readyState || this.readyState === "loaded" || this.readyState === "complete")) {
 			done = true;
@@ -125,7 +287,7 @@ function bookmarklet(window,document,origin) {
 	};
 	
 	// append iframe
-	body.insertBefore(iframe,body.firstChild);
+	document.body.appendChild(iframe);
 }
 
 // generate random cookie to prevent postMessage() spam, and
@@ -152,19 +314,22 @@ $('#add')
 	});
 
 /**
- * listen for incoming messages
+ * listen for incoming messages when in iframe mode
  */
-window.addEventListener("message", function(event) {
+if (window !== window.top && document.location.hash === '#' + key) {
+	window.addEventListener("message", function(event) {
 	
-	// serve script to anyone who requests it
-	if (event.data === "script") {
-		event.source.postMessage('(' + tenk.scanner + ')(window,document)', "*");
-		return;
-	}
+		// serve script to anyone who requests it
+		if (event.data === "script") {
+			event.source.postMessage('(' + tenk.scanner + ')(window,document)', "*");
+			return;
+		}
 	
-	// process index submission
+		// process index submission
+		alert(event.data);
 	
-}, false);
+	}, false);
+}
 
 })(window,document,jQuery,window['10kse']);
 
