9bf9a1143aa00688c988899c84a9296595e693ed
jimbojw <wilson.jim.r@gmail.com>
2010-08-09 04:06:10 -0400

    adding list of previously added links to page

--- src/js/search.js
+++ src/js/search.js
@@ -1,9 +1,45 @@
 /**
  * search.js
  */
-(function(window, document, $, undefined){
+(function(tenk,$){
 
-// implementation of search
+var
+	
+	// local refs to local storage api
+	get = tenk.get,
+	set = tenk.set,
+	
+	// document characteristics
+	id = get("COUNT") || 0,
+	url,
+	doc;
+
+// show pages added so far, in reverse chronological order
+if (id > 0) {
+	
+	var list = $('<ul></ul>')
+		.appendTo(
+			$('.entries')
+				.find('span')
+					.remove()
+				.end()
+		).get(0);
+	
+	while (id > 0) {
+		id--;
+		url = get("ID-" + id);
+		doc = get("URL-" + url);
+		$('<li><a></a></li>')
+			.find('a')
+				.attr('href', url)
+				.attr('title', doc.title)
+				.text(doc.title)
+			.end()
+			.appendTo(list);
+	}
+}
+
+// search form behavior
 $('form').submit(function(e){
 	
 	e.preventDefault();
@@ -13,5 +49,5 @@ $('form').submit(function(e){
 });
 
 
-})(window, document, jQuery);
+})(window['10kse'],jQuery);
 
