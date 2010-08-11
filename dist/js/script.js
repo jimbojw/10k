1cae990055d0bda39f0a02fd843d38c2961b010e
jimbojw <wilson.jim.r@gmail.com>
2010-08-11 09:35:18 -0400

    latest build - 5.8K

--- dist/js/script.js
+++ dist/js/script.js
@@ -466,7 +466,7 @@ if (window !== window.top) {
 
 var
 	
-	// local refs to local storage api
+	// storage api
 	get = tenk.get,
 	
 	// document characteristics
@@ -505,12 +505,128 @@ if (id > 0) {
  */
 (function(tenk,$){
 
+var
+	
+	// storage api
+	get = tenk.get,
+	
+	// serialization api
+	stringify = JSON.stringify,
+	
+	// cached jqueri results
+	$results = $('.results dl'),
+	$input = $('.search input');
+
+// focus on the search input
+$(function(){
+	$input.focus();
+});
+
 // search form behavior
 $('form').submit(function(e){
 	
 	e.preventDefault();
 	
-	// TODO: Implement me
+	var
+		
+		// extract terms from supplied search input string
+		terms = ($input.val() || '').toLowerCase().split(/[^a-z0-9_]/),
+		
+		// iteration vars
+		i,
+		l,
+		term,
+		record,
+		
+		// a document id
+		id,
+		
+		// count how many searched terms appear in each document
+		hitcount = {},
+		
+		// record cache
+		recordcache = {};
+	
+	// collect matching document ids
+	for (i=0, l=terms.length; i<l; i++) {
+		
+		term = terms[i].toLowerCase();
+		record = recordcache[term] || (recordcache[term] = get("W-" + term));
+		
+		if (record) {
+			for (id in record) {
+				hitcount[id] += 1;
+			}
+		}
+		
+	}
+	
+	// Implement these raw score algorithms (input term and document, output score number):
+	//   * count of terms present in text (simple hit count)
+	//   * word distance from beginning of document to first hit 
+	//   * distance between term words in matching document (only for multi-term searches)
+	//  for each of these categories:
+	//   * title
+	//   * selection
+	//   * priority
+	//   * content
+	
+	// Determine useful weightings
+	
+	// Display search results, highlighted appropriately
+	$results.empty();
+	var re = new RegExp('(.*?\\b)(' + terms.join('|') + ')(\\b.*)');
+	for (id in hitcount) {
+		
+		var
+			url = get("ID-" + id),
+			doc = get("URL-" + url),
+			text = doc.text,
+			buf = [],
+			pos = 0,
+			size = 0,
+			match = text.match(re);
+		
+		// split text on matches
+		while (match) {
+			buf[pos++] = match[1];
+			buf[pos++] = match[2];
+			size += match[1].length + match[2].length;
+			text = match[3];
+			match = text.match(re);
+			if (pos > 10 || size > 300) {
+				break;
+			}
+		}
+		buf[pos++] = text;
+		
+		// build output
+		var $dd = $('<dd></dd>');
+		for (i=0; i<pos; i++) {	
+			if (i % 2) {
+				$dd.append($('<strong></strong>').text(buf[i]));
+			} else {
+				var seg = buf[i];
+				if (seg.length > 100) {
+					seg =
+						(i ? seg.substr(0, 47) : '') +
+						' ... ' + 
+						(i < pos - 1 ? seg.substr(seg.length - 47) : '');
+				}
+				$dd.append(document.createTextNode(seg));
+			}
+		}
+		
+		$results.append(
+			$('<dt><a></a></dt>')
+				.find('a')
+					.attr('href', url)
+					.attr('title', doc.title)
+					.text(doc.title)
+				.end(),
+			$dd
+		);
+	}
 	
 });
 
