797098e64f4613a890029098874817c4f70b9db7
jimbojw <wilson.jim.r@gmail.com>
2010-08-15 19:31:45 -0400

    adding comments to drive future development

--- src/js/autocomplete.js
+++ src/js/autocomplete.js
@@ -4,12 +4,40 @@
 (function(window,tenk,$,undefined){
 
 /**
- * implementation of autocomplete.
+ * get array of suggestions based on input string.
+ * @param {string} query The value entered for which to find suggestions.
+ */
+function suggestions(query) {
+	
+	// TODO: implement me!!
+	
+}
+
+/**
+ * autocomplete instrumentation.
+ * @param {element} input The input element to autocomplete.
  */
 function autocomplete(input) {
+	
+	// create drop-down structure
+	
+	// capture keyup and change on input
+		
+		// clear timeout if already set
+		
+		// set timeout for duration, then update choices
+		
+	// update choices
+		
+		// get suggestions
+		
+		// fill in suggestions in drop-down
+		
+	
 }
 
 // exports
 tenk.autocomplete = autocomplete;
+tenk.suggestions = suggestions;
 
 })(window,window['10kse'],jQuery);
