a4d27683a88bc434c9df455c14a9f3b3875f7dd4
jimbojw <wilson.jim.r@gmail.com>
2010-08-20 04:52:16 -0400

    implementing prefs form behavior

--- src/js/prefs.js
+++ src/js/prefs.js
@@ -1,8 +1,44 @@
 /**
  * prefs.js - implements the prefs tab.
  */
-(function($){
+(function(tenk,$){
 
+var
+	
+	// storage api
+	get = tenk.get,
+	set = tenk.set,
+	
+	// cached jquery results
+	$prefs = $('.prefs.pane'),
+	$input = $prefs.find('input').eq(0),
+	
+	// prefered web search
+	s = get("S") || '';
 
+if (!s) {
+	s = $prefs.find('.def').attr('href');
+	set("S", s);
+}
+$input.val(s);
 
-})(jQuery);
+$prefs.find('li a').click(function(e){
+	
+	e.preventDefault();
+	
+	$input.val(this.href);
+	
+});
+
+$prefs.find('form').submit(function(e){
+	
+	e.preventDefault();
+	
+	s = $input.val();
+	set("S", s);
+	
+	alert('Preferences saved successfully!')
+	
+});
+
+})(window['10kse'],jQuery);
