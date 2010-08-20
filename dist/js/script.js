a55c8fa6462125b41d38b244d1e662601d20a25a
jimbojw <wilson.jim.r@gmail.com>
2010-08-20 05:10:49 -0400

    latest build - 7.8K crushed

--- dist/js/script.js
+++ dist/js/script.js
@@ -126,6 +126,60 @@ tenk.showtab = showtab;
 
 })(window['10kse'],jQuery);
 /**
+ * prefs.js - implements the prefs tab.
+ */
+(function(tenk,$){
+
+var
+	
+	// storage api
+	get = tenk.get,
+	set = tenk.set,
+	
+	// cached jquery results
+	$prefs = $('.prefs.pane'),
+	$input = $prefs.find('input').eq(0),
+	$saved = $prefs.find('.saved'),
+	
+	// prefered web search
+	s = get("S") || '';
+
+if (!s) {
+	s = $prefs.find('.def').attr('href');
+	set("S", s);
+}
+$input.val(s);
+
+$prefs.find('li a').click(function(e){
+	
+	e.preventDefault();
+	
+	$input.val(this.href);
+	
+});
+
+$prefs.find('form').submit(function(e){
+	
+	e.preventDefault();
+	
+	s = $input.val();
+	set("S", s);
+	
+	$saved.css({
+		visibility: '',
+		opacity: 1
+	});
+	
+	setTimeout(function(){
+		$saved.animate({
+			opacity: 0
+		}, 'slow');
+	}, 1000);
+	
+});
+
+})(window['10kse'],jQuery);
+/**
  * trie.js
  */
 (function(tenk,$,undefined){
@@ -1572,6 +1626,9 @@ function autocomplete(input) {
 		.mouseover(mouseover)
 		.click(select);
 	
+	// clear the autocompleter when clicking anywhere else
+	$('body').click(hide);
+	
 }
 
 // set up input box for autocompletion
@@ -1784,6 +1841,16 @@ function search() {
 $('form').submit(function(e){
 	e.preventDefault();
 	setTimeout(search, 100);
+});
+
+// implement web search button
+$('.search').find('input[name=web]').click(function(e){
+	window.location.assign(
+		(get("S") || '').replace(
+			'%s',
+			encodeURIComponent($input.val() || '')
+		)
+	);
 });
 
 // focus on the search input
