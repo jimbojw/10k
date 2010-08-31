175ee95ccb03cdb7eb7b24409ca47c2997400542
jimbojw <wilson.jim.r@gmail.com>
2010-08-31 14:48:30 -0400

    adding tests for new trie levenshtein distance function

--- test/unit/trie.js
+++ test/unit/trie.js
@@ -114,7 +114,7 @@ test("find node", function() {
 	expect(2);
 	
 	var
-		data = {
+		t = trie({
 			h:{
 				i:{
 					$:1
@@ -132,8 +132,7 @@ test("find node", function() {
 					}
 				}
 			}
-		},
-		t = trie(data);
+		});
 	
 	QUnit.deepEqual(
 		t.find("he"),
@@ -161,7 +160,7 @@ test("find matches", function() {
 	expect(5);
 	
 	var
-		data = {
+		t = trie({
 			h:{
 				e:{
 					y:{
@@ -182,8 +181,7 @@ test("find matches", function() {
 					$:1
 				}
 			}
-		},
-		t = trie(data);
+		});
 	
 	QUnit.deepEqual(t.match("miss"), [], "match 'miss'");
 	
@@ -194,6 +192,82 @@ test("find matches", function() {
 	QUnit.deepEqual(t.match("hey"), ['hey', 'heya'], "match 'hey'");
 	
 	QUnit.deepEqual(t.match("heya"), ['heya'], "match 'heya'");
+	
+});
+
+test("levenshtein", function() {
+	
+	var
+		
+		// trie object for testing
+		t = trie({
+			h:{
+				e:{
+					y:{
+						a:{
+							$:1
+						},
+						$:1
+					},
+					l:{
+						l:{
+							o:{
+								$:1
+							}
+						}
+					}
+				},
+				i:{
+					$:1
+				}
+			}
+		}),
+		
+		// tests to run
+		leventests = [
+		
+			// args, expected
+			[ ['miss', 2], [] ],
+			[ ['hi', 0], ['hi'] ],
+			[ ['hi', 1], ['hi'] ],
+			[ ['hi', 2], ['hey', 'hi'] ],
+			[ ['hi', 3], ['hey', 'heya', 'hi'] ],
+			[ ['key', 0], [] ],
+			[ ['key', 1], ['hey'] ],
+			[ ['key', 2], ['hey', 'heya'] ],
+			[ ['jello', 1], ['hello'] ],
+			[ ['jelly', 1], [] ],
+			[ ['jelly', 2], ['hello'] ],
+			[ ['is', 2], ['hi'] ],
+			[ ['I', 1], ['hi'] ],
+			[ ['eye', 2], ['hey', 'heya'] ]
+		
+		],
+		len = leventests.length,
+		
+		// iteration vars
+		i,
+		leventest,
+		args,
+		expected;
+	
+	expect(len + 1);
+	
+	ok(t.levenshtein, "t.levenshtein");
+	
+	for (i=0; i<len; i++) {
+		
+		leventest = leventests[i];
+		args = leventest[0];
+		expected = leventest[1];
+		
+		QUnit.deepEqual(
+			t.levenshtein.apply(t, args),
+			expected,
+			'(' + args.join(', ') + ')'
+		);
+		
+	}
 	
 });
 
